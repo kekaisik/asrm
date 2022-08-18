488e61408de68ea39a87cf6584ee6b3043221531
kekaisik <kakaisik0606@gmail.com>
2022-08-18 22:06:23 +0600

    kek

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -268,29 +268,29 @@ AUTH_USER_MODEL = 'acpm.User'
 CKEDITOR_UPLOAD_PATH = 'uploads/'
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'asrm',
-#         'USER': 'postgres',
-#         'PASSWORD': 'kszda2019',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
-
-
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'acpm',
+        'NAME': 'asrm',
         'USER': 'postgres',
-        'PASSWORD': '504142',
+        'PASSWORD': 'kszda2019',
         'HOST': 'localhost',
-        'PORT': '6000',
+        'PORT': '5432',
     }
 }
+
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'acpm',
+#         'USER': 'postgres',
+#         'PASSWORD': '504142',
+#         'HOST': 'localhost',
+#         'PORT': '6000',
+#     }
+# }
 
 db_from_env = dj_database_url.config()
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
